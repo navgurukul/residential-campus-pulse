bccb5a74de5817e838b7f122f28a535518071262
Suraj Sahani <surajsahani@navgurukul.org>
2026-02-13 12:58:04 +0530

    Fix Firebase initialization error - add isSupported check for analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Analytics only if supported (prevents errors in some environments)
+let analytics;
+isSupported().then(yes => {
+  if (yes) {
+    analytics = getAnalytics(app);
+  }
+}).catch(err => {
+  console.log('Analytics not supported:', err);
+});
+
+export { app, analytics };
